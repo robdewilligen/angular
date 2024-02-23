b5e7d24aa79ec72390437cf17b13252d05690da1
Rob de Willigen <robdewilligen@outlook.com>
2024-02-23 16:36:27 +0100

    add details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [],
-  template: `
-    <p>
-      details works!
-    </p>
-  `,
-  styleUrl: './details.component.scss'
+    selector: 'app-details',
+    standalone: true,
+    imports: [],
+    templateUrl: './details.component.html',
+    styleUrl: './details.component.scss'
 })
 export class DetailsComponent {
+    route: ActivatedRoute = inject(ActivatedRoute);
+    housingService = inject(HousingService);
+    housingLocation: HousingLocation | undefined;
 
+    constructor() {
+        const housingLocationId = Number(this.route.snapshot.params['id']);
+        this.housingLocation =
+            this.housingService.getHousingLocationById(housingLocationId);
+    }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,14 @@
-import {Component, Input} from '@angular/core';
-import {CommonModule} from "@angular/common";
-import {HousingLocation} from "../housinglocation";
+import { Component, Input } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { HousingLocation } from "../housinglocation";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-housing-location',
     standalone: true,
-    imports: [],
+    imports: [
+        RouterLink
+    ],
     templateUrl: 'housing-location.component.html',
     styleUrl: './housing-location.component.scss'
 })
