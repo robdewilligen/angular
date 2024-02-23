9993fbcf9edc234f5e4027ecb6b765a505512dd9
Rob de Willigen <robdewilligen@outlook.com>
2024-02-23 12:06:18 +0100

    create Input for housingLocation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
 
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {HousingLocation} from "../housinglocation";
 
 @Component({
-  selector: 'app-housing-location',
-  standalone: true,
-  imports: [],
-  templateUrl: 'housing-location.component.html',
-  styleUrl: './housing-location.component.scss'
+    selector: 'app-housing-location',
+    standalone: true,
+    imports: [],
+    templateUrl: 'housing-location.component.html',
+    styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
+    @Input() housingLocation!: HousingLocation;
 }
