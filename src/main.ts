d73fd2cbc0918a2c5e615560567be61e16416d7d
Rob de Willigen <robdewilligen@outlook.com>
2024-02-23 15:22:50 +0100

    small fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
@@ -7,7 +7,7 @@ import { routeConfig } from './app/app.routes'
 bootstrapApplication(AppComponent,
     {
         providers: [
-            // provideProtractorTestingSupport(),
+            provideProtractorTestingSupport(),
             provideRouter(routeConfig)
         ]
     }
