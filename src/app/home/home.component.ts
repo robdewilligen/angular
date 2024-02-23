7f9ddfa7f67ef66ae51acad816f7cf907b5ac1cf
Rob de Willigen <robdewilligen@outlook.com>
2024-02-23 14:49:40 +0100

    Add service and inject
    created a service to handle the housingLocations data. Injected the service into home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { HousingLocation } from '../housinglocation';
+import { NgForOf } from "@angular/common";
+import { HousingService } from "../housing.service";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [ HousingLocationComponent ],
+    imports: [HousingLocationComponent, NgForOf],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
     readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
-    housingLocation: HousingLocation = {
-        id: 9999,
-        name: 'Test Home',
-        city: 'Test city',
-        state: 'ST',
-        photo: `${this.baseUrl}/example-house.jpg`,
-        availableUnits: 99,
-        wifi: true,
-        laundry: false,
-    };
+    housingLocationList: HousingLocation[] = [];
+    housingService: HousingService = inject(HousingService);
+
+    constructor() {
+        this.housingLocationList = this.housingService.getAllHousingLocations();
+    }
 }
