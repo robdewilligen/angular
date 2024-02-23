914eae6fd96e8b408374eceba8c4a29ae2dbd041
Rob de Willigen <robdewilligen@outlook.com>
2024-02-23 17:30:27 +0100

    filter on searchbar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,9 +15,22 @@ export class HomeComponent {
     readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
     housingLocationList: HousingLocation[] = [];
+    filteredLocationList: HousingLocation[] = [];
     housingService: HousingService = inject(HousingService);
 
     constructor() {
         this.housingLocationList = this.housingService.getAllHousingLocations();
+        this.filteredLocationList = this.housingLocationList;
+    }
+
+    filterResults(text: string) {
+        if (!text) {
+            this.filteredLocationList = this.housingLocationList;
+            return;
+        }
+
+        this.filteredLocationList = this.housingLocationList.filter(
+            housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+        );
     }
 }
